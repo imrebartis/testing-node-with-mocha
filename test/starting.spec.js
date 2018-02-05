199a85ab2aea16cdb719e64432129a43391e204d
Imre Bartis <imre.bartis@siili.com>
2018-02-05 13:24:31 +0200

    first example of BDD testing

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -2,6 +2,6 @@ var assert = require('assert');
 
 describe('Basic Mocha Test', function () {
     it('should throw errors', function () {
-        assert.equal(2,3);
+        // assert.equal(2,3);
     })
 });
