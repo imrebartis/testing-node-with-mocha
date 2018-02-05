e395b42ada190f2128bc95746ea2671d751473c3
Imre Bartis <imre.bartis@siili.com>
2018-02-05 13:52:24 +0200

    testing async

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,12 @@ function AuthController(){
     function isAuthorized(roles, neededRole){
         return roles.indexOf(neededRole) >= 0; 
     }
-    return {isAuthorized};
+
+    function isAuthorizedAsync(roles, neededRole, cb){
+        setTimeout(function(){cb(roles.indexOf(neededRole) >= 0)}, 0);  
+    }
+
+    return {isAuthorized, isAuthorizedAsync};
 }
 
 module.exports = AuthController();
--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -3,15 +3,26 @@ var authController = require('../../controllers/auth.controller');
 
 
 
-describe('AuthController', function () {
+describe('AuthController', function() {
 
-    describe('isAuthorized', function () {
+    describe('isAuthorized', function() {
 
-        it('Should return false if not authorized', function () {
+        it('Should return false if not authorized', function() {
             assert.equal(false, authController.isAuthorized(['user'],'admin'));
         })
-        it('Should return true if authorized', function () {
+        it('Should return true if authorized', function() {
             assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'));
+        })
+    })
+
+    describe('isAuthorizedAsync', function() {
+
+        it('Should return false if not authorized', function(done) {
+                authController.isAuthorizedAsync(['user'], 'admin'
+                , function(isAuth){
+                    assert.equal(false, isAuth);
+                    done();
+                });
         })
     })
 
